bd31ccb2330e204688b0946e76423349dd1df7a7
Gabriella <gabriellalok3@gmail.com>
2023-03-03 03:05:47 -0300

    buscando uma pessoa no banco de dados usando find

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,24 +8,24 @@ import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
-
-        Pessoa p1 = new Pessoa(null, "Gabriella Ferreira", "gabriella@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Amanda Ferreira", "amanda@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Jordania Ferreira", "jordania@gmail.com");
+            //objetos já salvos no banco de dados
+//        Pessoa p1 = new Pessoa(null, "Gabriella Ferreira", "gabriella@gmail.com");
+//        Pessoa p2 = new Pessoa(null, "Amanda Ferreira", "amanda@gmail.com");
+//        Pessoa p3 = new Pessoa(null, "Jordania Ferreira", "jordania@gmail.com");
 
         //pega o nome do arquivo persistence.xml
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         //conexao com o banco de dados e todos os contexto de persistencia implementada
         EntityManager em = emf.createEntityManager();
 
-        //salvar os objetos no banco de dados
-        em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+        //buscando uma pessoa no banco de dados
+        Pessoa p = em.find(Pessoa.class, 3);
 
+        System.out.println(p);
         System.out.println("Feito!");
+
+        em.clear();
+        emf.close();
 
 
     }
