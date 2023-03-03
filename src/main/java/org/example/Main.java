ff964e9d77c3f41ad72ebc1887c7c02b87e135f3
Gabriella <gabriellalok3@gmail.com>
2023-03-03 02:34:45 -0300

    configurando o arquivo pom.xml com as dependencias do hibernate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import dominio.Pessoa;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Pessoa p1 = new Pessoa(1, "Gabriella Ferreira", "gabriella@gmail.com");
+        Pessoa p2 = new Pessoa(1, "Amanda Ferreira", "amanda@gmail.com");
+        Pessoa p3 = new Pessoa(1, "Jordania Ferreira", "jordania@gmail.com");
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+
     }
 }
